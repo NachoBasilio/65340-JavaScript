055565d458fa5b52b25d953f385d37df297ef7e5
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2024-10-15 20:54:53 -0300

    terminada la clase 1

---
+++ Clase-1/main.js
@@ -0,0 +1,52 @@
+// console.log("hola!")
+
+// let nombre = "Rengar"
+
+// let edad
+
+// edad = NaN
+
+// let casado = false
+
+// let jose = null
+
+// console.log(jose)
+
+// console.log(typeof edad)
+
+// console.log(isNaN(edad))
+
+// console.log("22" / 2)
+
+// alert("Hola!")
+
+// let edad = parseInt(prompt("¿Cuantos años tenes?"))
+
+// console.log(edad)
+
+// let factura = confirm("¿esta seguro que quiere facturar?")
+
+// console.log(factura)
+
+// const pi = 3.14
+// const URL = "www.google.com"
+
+//IMC
+
+// let peso = parseFloat(prompt("¿Cuanto pesa usted? en Kg"))
+
+// let altura = Number(prompt("¿Cuanto mide usted? en Metros"))
+
+// console.log("usted pesa: " + peso + " usted mide: " + altura )
+
+// let imc = peso / (altura ** 2)
+
+// alert("Tu indice de Masa Corporal es de: " + imc.toFixed(2) + "!!!!!!!!!!!!")
+
+// De C a F
+
+// let celsius = Number(prompt("Ingrese la temperatura\nen grados Celsius: "))
+
+// let fahrenheit = (celsius * 1.8) + 32
+
+// alert("La temperatura en Fahrenheit es: " + fahrenheit)
